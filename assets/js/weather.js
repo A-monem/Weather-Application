6453bc87985219b29f33560b53451b5ed2fd5335
A-monem <abdulmonemamin@hotmail.com>
2019-02-24 01:12:19 +1100

    Javascript initial

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,2 +1,24 @@
+window.addEventListener("load", () => {
+    let long;
+    let lat;
 
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position) => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const api = `${proxy}https://api.darksky.net/forecast/28219da3e847c1437eeece08ebfe1ac2/${lat},${long}`
+            
+            fetch(api)
+                .then(responce => {
+                console.log(responce.json())
+            })
+            
+
+        });
+
+
+    };
+
+});
 
