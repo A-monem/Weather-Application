5b0018c0277c8a12186fd6c68a4db1942a6d78e2
A-monem <abdulmonemamin@hotmail.com>
2019-02-24 03:05:39 +1100

    adding skycons script

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,24 +1,36 @@
 window.addEventListener("load", () => {
     let long;
     let lat;
+    let tempratueDegree = document.querySelector(".temprature-degree")
+    let tempratueDescription = document.querySelector(".temprature-description")
+    let locationTimezone = document.querySelector(".location-timezone")
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((position) => {
+            // get current location longitude and latitude 
             long = position.coords.longitude;
             lat = position.coords.latitude;
             const proxy = "https://cors-anywhere.herokuapp.com/"
             const api = `${proxy}https://api.darksky.net/forecast/28219da3e847c1437eeece08ebfe1ac2/${lat},${long}`
-            
+            // api request to darksky
             fetch(api)
                 .then(responce => {
-                console.log(responce.json())
-            })
-            
-
+                return responce.json();
+                }) 
+                .then(data => {
+                    console.log(data);
+                    const {temperature, summary} = data.currently;
+                    // set DOM elements from the api
+                    tempratueDegree.textContent = temperature;
+                    tempratueDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;  
+                });
         });
-
-
     };
 
+    function setIcon(icon, iconID){
+        var skycons = new Skycons({"color": "white"});
+
+    }
 });
 
